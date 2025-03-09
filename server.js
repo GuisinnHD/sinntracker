74254c8bc196e80ac6f8920277ff7e579b286d6d
Guilherme <farmdoguisinn07@gmail.com>
2025-03-09 17:51:55 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,31 +4,40 @@ const port = process.env.PORT || 3000;
 
 // Simulação de dados que você obteria da API do Duolingo
 const getDuolingoData = (username) => {
-    return {
-        users: [
-            {
-                username: username,
-                name: "Guilherme ⚡️",
-                streak: 321,
-                motivation: "travel",
-                totalXp: 16312,
-                picture: "//simg-ssl.duolingo.com/ssr-avatars/1438369258/SSR-9sXmVGCcBz",
-                courses: [
-                    { title: "Japanese", xp: 3733, crowns: 9999 },
-                    { title: "English", xp: 9101, crowns: 9999 },
-                    { title: "Korean", xp: 3478, crowns: 9999 }
-                ]
-            }
-        ]
-    };
+    // Simulação de dados de um perfil do Duolingo
+    if (username === 'Guisinn') {
+        return {
+            users: [
+                {
+                    username: username,
+                    name: "Guilherme ⚡️",
+                    streak: 321,
+                    motivation: "travel",
+                    totalXp: 16312,
+                    picture: "//simg-ssl.duolingo.com/ssr-avatars/1438369258/SSR-9sXmVGCcBz",
+                    courses: [
+                        { title: "Japanese", xp: 3733, crowns: 9999 },
+                        { title: "English", xp: 9101, crowns: 9999 },
+                        { title: "Korean", xp: 3478, crowns: 9999 }
+                    ]
+                }
+            ]
+        };
+    } else {
+        // Retorna um erro se o usuário não for encontrado
+        return { error: "Usuário não encontrado!" };
+    }
 };
 
 // Rota para buscar informações de um perfil do Duolingo
 app.get('/duolingo/:username', (req, res) => {
     const username = req.params.username;
-
-    // Simula a busca de dados do Duolingo
     const userData = getDuolingoData(username);
+
+    // Verifica se existe o erro (caso o usuário não exista)
+    if (userData.error) {
+        return res.status(404).json(userData);
+    }
 
     // Retorna os dados como JSON
     res.json(userData);
